2ae49a389e59f7e0ba105cd3e3af1833461ead78
SERG <40707060+LEWISGREGG@users.noreply.github.com>
2021-07-05 17:33:02 -0400

    Add files via upload

--- src/components/StockCrawler.js
+++ src/components/StockCrawler.js
@@ -3,7 +3,7 @@ import StockCrawler from 'react-stock-crawler/dist';
 function Crawler() {
   return (
    <div className="StockCrawler">
-    <StockCrawler stocks={['FTXP', 'JAMN', 'SUTI', 'AHFD', 'FONU', 'EFIR', 'VGID', 'NBRI', 'BCAP', 'PRMO', 'SEEK', 'GEGP', 'ICTY', 'TXTM', 'AFOM', 'WSFT', 'WRFX', 'ASKE', 'GMPW', 'NUUU', 'NNRX']} apiKey={"c1ehs7748v6pretcu1gg"} />
+    <StockCrawler stocks={['FTXP', 'JAMN', 'SUTI', 'TGRO', 'FONU', 'EFIR', 'VGID', 'NBRI', 'BCAP', 'BEHL', 'SEEK', 'SDVI', 'ICTY', 'ITVI', 'TWOH', 'RDAR', 'WRFX', 'PDGO', 'FUTL', 'NUUU', 'NNRX']} apiKey={"c1ehs7748v6pretcu1gg"} />
    </div>
   );
 }
